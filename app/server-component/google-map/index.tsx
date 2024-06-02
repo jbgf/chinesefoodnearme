c122c460381e265eef8b93441a9a84a9896f2628
Thomas Wade <w1220w@126.com>
2024-06-02 22:23:30 +0800

    feat: location

--- app/server-component/google-map/index.tsx
+++ app/server-component/google-map/index.tsx
@@ -1,14 +1,30 @@
+'use client';
 import { GoogleMapsEmbed } from '@next/third-parties/google'
 import { GOOGLE_MAP_API_KEY } from './const'
+import { useEffect, useState } from 'react';
 
 export default function GoogleMap() {
+  const [location, setLocation] = useState<string>();
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setLocation(`${latitude},${longitude}`);
+      });
+    }
+  }, []);
+
+  console.log(location, 'location');
+
   return (
     <GoogleMapsEmbed
       apiKey={GOOGLE_MAP_API_KEY}
       height={400}
       width="100%"
-      mode="place"
-      q="Brooklyn+Bridge,New+York,NY"
+      mode="search"
+      q="chinese+food" //+in+NewYork
+      center={location}
+      zoom={`10`}
     />
   )
 }
